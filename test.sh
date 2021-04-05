ddffbc8cb6fa8960d63d0b407b4d51aa14463ff2
Samuel C Moore <samuel.c.moore1221@gmail.com>
2021-04-05 17:24:59 -0500

    edditing test.sh to test remote commands

--- test.sh
+++ test.sh
@@ -2,19 +2,9 @@
 # example
 # ./deploy.sh example.com example
 DOMAIN=$1
-APP_NAME=$2
-DB_NAME=$2_db
-
-#ssh root@${DOMAIN} bash <<setup_dokku
-#pwd
-#cd /var/lib/dokku/services/mysql/${DB_NAME}
-#pwd
-#ls
-#cat ROOTPASSWORD
-#MYSQL_ROOT_PASSWORD=$(< ROOTPASSWORD)
-#setup_dokku
-
-MYSQL_ROOT_PASSWORD=$(ssh root@${DOMAIN} cat /var/lib/dokku/services/mysql/$DB_NAME/ROOTPASSWORD)
-
-echo $MYSQL_ROOT_PASSWORD
-
+ssh root@$DOMAIN bash << setup_dokku
+sudo echo hello
+dokku apps:list
+setup_dokku
+#MYSQL_ROOT_PASSWORD=$(ssh root@${DOMAIN} cat /var/lib/dokku/services/mysql/$DB_NAME/ROOTPASSWORD)
+#echo $MYSQL_ROOT_PASSWORD

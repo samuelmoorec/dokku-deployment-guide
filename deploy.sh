a8b253324ea6c3d40edc1b2c4bc458629901dc14
Samuel C Moore <samuel.c.moore1221@gmail.com>
2021-04-05 20:56:34 -0500

    removed letsencrypt

--- deploy.sh
+++ deploy.sh
@@ -18,15 +18,16 @@ dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
 dokku apps:create $APP_NAME
 dokku mysql:create $DB_NAME
 dokku mysql:link $DB_NAME $APP_NAME
-dokku config:set --no-restart "$APP_NAME" SERVER_PORT=$PORT DOKKU_LETSENCRYPT_EMAIL=$EMAIL
+dokku config:set --no-restart "$APP_NAME" DOKKU_LETSENCRYPT_EMAIL=$EMAIL
 dokku domains:add $APP_NAME $DOMAIN
 dokku domains:remove $APP_NAME $APP_NAME
-dokku letsencrypt:enable $APP_NAME
-dokku letsencrypt:auto-renew $APP_NAME
 setup_dokku
 
-MYSQL_ROOT_PASSWORD=$(ssh root@${IPADDRESS} cat /var/lib/dokku/services/mysql/$DB_NAME/ROOTPASSWORD)
-git remote add dokku dokku@$IPADDRESS:$APP_NAME
+#dokku letsencrypt:enable $APP_NAME
+#dokku letsencrypt:auto-renew $APP_NAME
+
+#MYSQL_ROOT_PASSWORD=$(ssh root@${IPADDRESS} cat /var/lib/dokku/services/mysql/$DB_NAME/ROOTPASSWORD)
+git remote add dokku dokku@$DOMAIN:$APP_NAME
 [[ $? -eq 0 ]] && echo "Dokku git remote created. Commit the Procfile and system.properties file and run git push dokku master."
 git add Procfile system.properties
 git commit -m "feat: Add Procfile and system.properties for deployment"

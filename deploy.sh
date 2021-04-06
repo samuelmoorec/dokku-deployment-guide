03ba00919c19ace6c0686509a92735da5b7da8ac
Samuel C Moore <samuel.c.moore1221@gmail.com>
2021-04-05 20:22:01 -0500

    fixed letsencrypt command

--- deploy.sh
+++ deploy.sh
@@ -18,15 +18,15 @@ dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
 dokku apps:create $APP_NAME
 dokku mysql:create $DB_NAME
 dokku mysql:link $DB_NAME $APP_NAME
-dokku config:set --no-restart "$APP_NAME" SPRING_DATASOURCE_URL=$JDBC_DATABASE_URL SPRING_JPA_HIBERNATE_DDL-AUTO=update SPRING_JPA_SHOW-SQL=true SERVER_PORT=$PORT DOKKU_LETSENCRYPT_EMAIL=$EMAIL
+dokku config:set --no-restart "$APP_NAME" SERVER_PORT=$PORT DOKKU_LETSENCRYPT_EMAIL=$EMAIL
 dokku domains:add $APP_NAME $DOMAIN
 dokku domains:remove $APP_NAME $APP_NAME
-dokku letsencrypt $APP_NAME
+dokku letsencrypt:enable $APP_NAME
 dokku letsencrypt:auto-renew $APP_NAME
 setup_dokku
 
 MYSQL_ROOT_PASSWORD=$(ssh root@${IPADDRESS} cat /var/lib/dokku/services/mysql/$DB_NAME/ROOTPASSWORD)
-git remote add dokku dokku@$DOMAIN:$APP_NAME
+git remote add dokku dokku@$IPADDRESS:$APP_NAME
 [[ $? -eq 0 ]] && echo "Dokku git remote created. Commit the Procfile and system.properties file and run git push dokku master."
 git add Procfile system.properties
 git commit -m "feat: Add Procfile and system.properties for deployment"
